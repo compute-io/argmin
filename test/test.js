72d85b1844b1924da0a4e8a995dba926ee2b530f
kgryte <kgryte@gmail.com>
2015-05-15 23:13:33 -0700

    [UPDATE] dotfiles. Makefile. license info. add accessor option. validation deps. [TESTS] accessor.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/* global require, describe, it */
 'use strict';
 
 // MODULES //
@@ -45,14 +46,63 @@ describe( 'compute-argmin', function tests() {
 		}
 	});
 
+	it( 'should throw an error if provided an accessor argument which is not a function', function test() {
+		var values = [
+			'5',
+			5,
+			true,
+			undefined,
+			null,
+			NaN,
+			[],
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				argmin( [1,2,3], value );
+			};
+		}
+	});
+
 	it( 'should compute the minimum value and return the corresponding indices', function test() {
 		var data, expected, actual;
 
 		data = [ 4, 2, 5, 3, 8, 2 ];
-		expected = [ 1, 5 ];
+
 		actual = argmin( data );
+		expected = [ 1, 5 ];
 
 		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should compute the minimum value and return the corresponding indices using an accessor', function test() {
+		var data, expected, actual;
+
+		data = [
+			{'x':4},
+			{'x':2},
+			{'x':5},
+			{'x':3},
+			{'x':8},
+			{'x':2}
+		];
+
+		actual = argmin( data, getValue );
+		expected = [ 1, 5 ];
+
+		assert.deepEqual( actual, expected );
+
+		function getValue( d ) {
+			return d.x;
+		}
+	});
+
+	it( 'should return null if provided an empty array', function test() {
+		assert.isNull( argmin( [] ) );
 	});
 
 });
